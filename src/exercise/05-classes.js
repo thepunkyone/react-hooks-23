6aafc5ad4464fe20b494474567456c0054d61c62
Whitney Bluzma <whitney@accommodationforstudents.com>
2023-02-23 12:20:46 +0000

    fix: remove current property from useEffect cleanup

--- src/exercise/05-classes.js
+++ src/exercise/05-classes.js
@@ -22,7 +22,7 @@ function Tilt({children}) {
     }
     VanillaTilt.init(tiltNode, vanillaTiltOptions)
 
-    return () => tiltRef.current.vanillaTilt.destroy()
+    return () => tiltNode.vanillaTilt.destroy()
   }, [])
 
   return (
